7d81d86c7f5efbb4b2138ec48de97288db669d9b
Anjel <anzhel.pak@gmail.com>
2024-03-02 12:45:23 -0600

    features/swiper-slide-animations

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-navbar = document.querySelector('.navbar');
-navbar__link = navbar.querySelectorAll('.navbar__link');
-navbar__link_options = navbar.querySelectorAll('.navbar__link_options');
-navbar__link_id = -1;
+let navbar = document.querySelector('.navbar');
+let navbar__link = navbar.querySelectorAll('.navbar__link');
+let navbar__link_options = navbar.querySelectorAll('.navbar__link_options');
+let navbar__link_id = -1;
 
 navbar__link.forEach(element => {
     element.addEventListener('mouseover', () => {
@@ -16,3 +16,36 @@ navbar__link.forEach(element => {
         navbar__link_options[element.id].classList.add('hidden');
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    var slides = document.querySelectorAll('.section__swiper-slide');
+    var currentSlide = 0;
+    var slideInterval;
+
+    function showSlide(index) {
+        slides[currentSlide].classList.remove('active');
+        slides[(currentSlide - 1 + slides.length) % slides.length].classList.remove('previous');
+        slides[(currentSlide + 1) % slides.length].classList.remove('next');
+
+        currentSlide = (index + slides.length) % slides.length;
+
+        slides[currentSlide].classList.add('active');
+        slides[(currentSlide - 1 + slides.length) % slides.length].classList.add('previous');
+        slides[(currentSlide + 1) % slides.length].classList.add('next');
+    }
+
+    function startSlider() {
+        slideInterval = setInterval(function() {
+            showSlide(currentSlide + 1);
+        }, 6000); // Change slide every 3 seconds
+    }
+
+    function stopSlider() {
+        clearInterval(slideInterval);
+    }
+
+    startSlider();
+});
+
+
+
