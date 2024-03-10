68be8ed5622f2113c3ba7029cdbb18b593248151
Anjel <anzhel.pak@gmail.com>
2024-03-10 00:44:40 -0600

    features/company-values-page

--- js/navbar.js
+++ js/navbar.js
@@ -3,6 +3,11 @@ let navbar__link = navbar.querySelectorAll('.navbar__link');
 let navbar__link_options = navbar.querySelectorAll('.navbar__link_options');
 let navbar__link_option = navbar.querySelectorAll('.navbar__link_option');
 let navbar__link_id = -1;
+let navbar__link_mobile = navbar.querySelector('.navbar__links-mobile');
+let navbar__link_mobile_button = navbar__link_mobile.querySelector('.navbar__links-mobile_icon');
+let navbar__link_mobile_container = navbar__link_mobile.querySelector('.navbar__links-mobile_container');
+let navbar__link_mobile_options = navbar__link_mobile.querySelectorAll('.navbar__links-mobile_option');
+let navbar__links_mobile_lists_button = navbar__link_mobile.querySelector('.navbar__links-mobile_lists_button');
 
 window.addEventListener('scroll', function() {
     var navbar = document.querySelector('.navbar');
@@ -41,5 +46,22 @@ navbar__link.forEach(element => {
 
     element.addEventListener('mouseout', () => {
         navbar__link_options[element.id].classList.add('hidden');
+    });
+});
+
+navbar__link_mobile_button.addEventListener('click', () => {
+    navbar__link_mobile_container.classList.remove('antimatter');
+});
+
+navbar__links_mobile_lists_button.addEventListener('click', () => {
+    navbar__link_mobile_container.classList.add('antimatter');
+})
+
+
+//finish
+navbar__link_mobile_options.forEach(element => {
+    element.addEventListener('click', () => {
+        console.log(element)
+        window.location.href = `https://${element.getElementsByTagName('a')[0].getAttribute("href")}`;
     });
 });
